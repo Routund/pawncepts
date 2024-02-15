38f602944584a9648c29efda9b9327a37da96fae
Wigg <routundity@gmail.com>
2024-02-15 13:06:57 +1300

    added wrapping

--- Scripts/Speech_manager.gd
+++ Scripts/Speech_manager.gd
@@ -19,12 +19,6 @@ var cust_variance = [
 	0.7, # Clueless, give high price, and have relatively low min price
 	1, # Veteran, give low price, and have relatively high min price   
 ]
-var items = [
-	0, # Necklace
-	1, # Ring
-	2, # Console
-	3, # Vase
-]
 var items_dict = {
 	0:"Necklace",
 	1:"Ring",
@@ -51,7 +45,7 @@ func _process(delta):
 
 
 func _on_customer_entered():
-	item = items[randi() % items.size()]
+	item = randi() % items_dict.size()
 	cust = customerTypes[randi() % customerTypes.size()]
 	min_price = int(prices[item] * (cust_variance[cust] + rng.randf_range(-0.2, 0.2)))
 	initial_price = min_price + randi_range(10, 50)  # Set an initial price above min_price
@@ -61,11 +55,11 @@ func _on_customer_entered():
 	# Display dialogue based on customer type
 	match cust:
 		0:
-			dialogue.text = "I'm looking to sell this %s. It's worth %d. Any objections?" % [items[item],initial_price]
+			dialogue.text = "I'm looking to sell this %s. It's worth $%d. Any objections?" % [items_dict[item],initial_price]
 		1:
-			dialogue.text = "Look man, you think this %s, is worth at least %d?" % [items[item],initial_price]
+			dialogue.text = "Look man, you think this %s, is worth at least $%d?" % [items_dict[item],initial_price]
 		2:
-			dialogue.text = "I'm not really sure what this %s is worth. would %d work?" % [items[item],initial_price]
+			dialogue.text = "I'm not really sure what this %s is worth. would $%d work?" % [items_dict[item],initial_price]
 		3:
-			dialogue.text = "I've done my own appraisal, and this %s is worth %d" % [items[item],initial_price]
+			dialogue.text = "I've done my own appraisal, and this %s is worth $%d" % [items_dict[item],initial_price]
 	pass

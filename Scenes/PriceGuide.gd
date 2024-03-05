1e82a89b75d6e5ef0d1c120a80422979789668e7
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-06 10:25:22 +1300

    Worked on inventory guide

---
+++ Scenes/PriceGuide.gd
@@ -0,0 +1,24 @@
+extends TextureButton
+
+var open = false
+@onready var pamphlet = get_node("TextureRect")
+var justPressed=false;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(Input.is_action_just_pressed("Pamphlet")):
+		open=!open
+		pamphlet.visible=!pamphlet.visible
+	elif (justPressed and !open) and not is_pressed():
+		# Button was pressed and is now released
+		justPressed = false
+		open=!open
+		pamphlet.visible=!pamphlet.visible
+	elif is_pressed() and not justPressed:
+		# Button is pressed for the first time
+		justPressed = true
+	pass
